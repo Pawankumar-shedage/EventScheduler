1f8dfefe1e396d730856a3a1b9133990cc1c85e0
Pawankumar Shedage <pawankumarshedage@gmail.com>
2024-11-06 12:01:41 +0530

    setup

--- src/main/java/com/event_scheduler/config/SecurityConfig.java
+++ src/main/java/com/event_scheduler/config/SecurityConfig.java
@@ -8,6 +8,12 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
+    // get username and password        UserDetails{}
+    // convert raw password -> hashed password      BcryptPassword()
+    // compare passwords
+    // return true or false acess granted/denied
+    // which urls to protect    configured in SecurityFilterChain
+    // which roles to allow    configured in authorizeHttpRequests
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
